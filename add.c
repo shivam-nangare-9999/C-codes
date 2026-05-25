b3253caaf935146d284236be21bab154b3179dd2
shivam-nangare-9999 <shivamnangare63@gmail.com>
2026-05-25 21:55:52 +0530

    added c program

---
+++ add.c
@@ -0,0 +1,15 @@
+//2 Number Addition
+
+#include<stdio.h>
+int main(int argc, char const *argv[])
+{
+    int a, b;
+    printf("Enter a Number");
+    scanf("%d",&a);
+
+    printf("Enter a Number");
+    scanf("%d",&b);
+    
+    printf("Sum is %d",a+b);
+    return 0;
+}
