7284a8a733212bc69ba6a54871550ca43f86d561
shivam-nangare-9999 <shivamnangare63@gmail.com>
2026-05-27 11:09:50 +0530

    float code

---
+++ float.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+    float a,b;
+    a=10.0;
+    b=20.0;
+    printf("a+b=%f\n",a+b);
+    printf("a-b=%f\n",a-b);
+    printf("a*b=%f\n",a*b);
+    printf("a/b=%f\n",a/b);
+   
+    return 0;
+}
