24c572cceee55760a45a7a832bd7057a4c4c8d91
shivam-nangare-9999 <shivamnangare63@gmail.com>
2026-05-27 22:21:43 +0530

    fixed multiplication table program

---
+++ ex1.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+ int main()
+{
+    int a;
+    printf("Enter The Number You Want Multiplication Table Of: ");
+    scanf("%d",&a);
+
+    for(int i=1;i<=10;i++)
+    {
+        printf("%2d x %2d = %3d\n",a,i,a*i);
+    }
+    return 0;
+}
