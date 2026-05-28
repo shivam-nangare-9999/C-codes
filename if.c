a45af4090f6877eee19ac754002cede22ebb0bb7
Shivam Nangare <shivamnangare63@gmail.com>
2026-05-28 14:27:49 +0530

    Update if.c

--- if.c
+++ if.c
@@ -5,11 +5,13 @@ int main()
     printf("Enter Your Age: ");
     scanf("%d", &age);
     printf("You Have Entered %d\n",age);
-    if(age>=18){
-        
+    
+    if(age>=18)
+    { 
         printf("You Can Vote\n");
     }
-    else{
+    else
+    {
         printf("You Cannot Vote\n");
     }
     return 0;
