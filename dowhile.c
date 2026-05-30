f7a695bd3eb245beca365e643277c48ff38e310b
shivam-nangare-9999 <shivamnangare63@gmail.com>
2026-05-30 21:04:26 +0530

    added break statement practice

---
+++ dowhile.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+    int i=0;
+    do
+    {
+        i=i+1;
+        printf("%d\n", i);
+    }while (i<20);
+
+    return 0;
+
+}
---
+++ switchcase.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+
+int main()
+{
+    int age;
+   printf("Enter Your Age\n");
+   scanf("%d", &age);
+
+   switch (age)
+   {
+   case 18:
+       printf("You are 18 years old.\n");
+       break;
+   case 19:
+       printf("You are 19 years old.\n");
+       break;
+   default:
+       printf("You are not 18 or 19 years old.\n");
+       break;
+   }
+
+    return 0;
+}
