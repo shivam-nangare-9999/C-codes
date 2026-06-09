a35b5b105690d106bd8f427af3438d42280a25db
shivam-nangare-9999 <shivamnangare63@gmail.com>
2026-06-09 16:48:53 +0530

    added recursion and exercise programs

---
+++ ex3.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+int main(int argc, char const *argv[])
+{
+   int num;
+   printf("enter the number you want to multiplication of: \n");
+   scanf("%d", &num);
+   
+
+   printf("the multiplication table of %d is: \n", num);
+
+   for (int i=1; i<=10; i++){
+         printf("%d x %d =%d\n", num, i, num*i);
+   }
+    return 0;
+}
