6327e8b1ce5fa23809ae2bf044b9b77649ed6343
Shivam Nangare <shivamnangare63@gmail.com>
2026-06-26 18:05:55 +0530

    Update logical.c

--- logical.c
+++ logical.c
@@ -1,5 +1,4 @@
 #include<stdio.h>
-
 int main()
 {
     int a,b;
@@ -9,6 +8,5 @@ int main()
     printf("a&&b=%d\n",a&&b);
     printf("a||b=%d\n",a||b);
     printf("a!b=%d\n",!(a&&b));
-   
     return 0;
 }
