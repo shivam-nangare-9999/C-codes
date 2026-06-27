145ab42135309f4772f3e7d4654c2a4ffe46c25e
shivam-nangare-9999 <shivamnangare63@gmail.com>
2026-06-27 16:25:26 +0530

    added ASCII program

---
+++ ASCII.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+    char ch= 'a';
+    printf("Enter a character: ");
+    scanf(" %c", &ch);
+    printf("The ASCII value of %c is %d \n", ch, ch);
+
+    if(ch >=97 && ch<=122){
+        printf("The Character is lower case \n");
+    }
+    else{
+        printf("The Character is upper case \n");
+    }
+    return 0;
+}
