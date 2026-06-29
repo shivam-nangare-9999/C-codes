085148293f739485f8f0557658ec9b812342405f
shivam-nangare-9999 <shivamnangare63@gmail.com>
2026-06-29 16:52:35 +0530

    do while loop natural number quick quiz

---
+++ dowhile2.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main()
+{
+    int i = 0;
+    do{
+        printf("Happy Birthday Shivam\n", i);
+        i++;
+    }
+    while(i<4);
+    return 0;
+}
---
+++ while2.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+    int i=0;
+    while(i<20)
+{
+    printf("Happy Birthday Shivam\n",i);
+    i++;
+}
+return 0;
+}
