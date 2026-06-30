764524541e5d2e2b6f726897051f6b64a1e58b40
shivam-nangare-9999 <shivamnangare63@gmail.com>
2026-06-30 23:08:45 +0530

    natural number using do while loop

---
+++ natural_number_for_dowhile.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+    int i = 1;
+    int sum = 0;
+   do{
+        sum += i;
+        i++;
+    } while(i<=10);
+    printf("Sum of first natural numbers is: %d\n", sum);
+    return 0;
+}
