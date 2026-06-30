ad19d982b5d295b8891abf6748f4cc9e242fbbe5
shivam-nangare-9999 <shivamnangare63@gmail.com>
2026-06-30 22:38:44 +0530

    Table Multiplication program added

---
+++ reversemultiplication.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+    int i;
+    int j;
+    printf("Enter Table Number:");
+    scanf("%d",&i);
+    for(j=10;j>=1;j--){
+        printf("%d x %d = %d\n",i,j,i*j);
+    }
+    return 0;
+}
