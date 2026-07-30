9e502e4a6409a684ef6bca7fe9af135895d41e60
shivam-nangare-9999 <shivamnangare63@gmail.com>
2026-07-30 09:54:31 +0530

    sum of natural no added

---
+++ Ch5_Functions_&_Recursion/quiz6.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int sum_natural(int);
+
+int sum_natural(int n)
+{
+    if (n == 1)
+    {
+        return 1;
+    }
+
+    return sum_natural(n - 1) + n;
+}
+
+int main()
+{
+    printf("The sum of 5 natural numbers is %d\n", sum_natural(5));
+
+    return 0;
+}
