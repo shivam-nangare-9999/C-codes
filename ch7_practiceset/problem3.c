d0366ef46e38b1d37bd09d80e2dbf71076e75c91
shivam-nangare-9999 <shivamnangare63@gmail.com>
2026-08-01 12:03:11 +0530

    problem 3 added

---
+++ ch7_practiceset/problem3.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main()
+{
+    int n;
+
+    printf("Enter a number: ");
+    scanf("%d", &n);
+
+    int arr[10];
+
+    for(int i = 0; i < 10; i++)
+    {
+        arr[i] = n * (i + 1);
+    }
+
+    for(int i = 0; i < 10; i++)
+    {
+        printf("%d x %d = %d\n", n, i + 1, arr[i]);
+    }
+
+    return 0;
+}
